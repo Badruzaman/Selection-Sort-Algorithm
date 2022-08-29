16f404cf5757dd10a7408a39880e8c4ce30f1b80
badruzzaman <badruzzaman@DESKTOP-EHGT2P0>
2022-08-29 10:43:53 +0600

    Add project files.

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] array = new int[] { 8, 5, 2, 9, 5, 6, 3 };
+int[] result = SelectionSort(array);
+foreach (var item in result)
+{
+    Console.Write(" " + item);
+}
+Console.ReadKey();
+
+int[] SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int smallIdx = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[smallIdx] > array[j])
+            {
+                smallIdx = j;
+            }
+        }
+        swap(i, smallIdx, array);
+
+    }
+    return array;
+}
+
+void swap(int i, int j, int[] array)
+{
+    int temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}
